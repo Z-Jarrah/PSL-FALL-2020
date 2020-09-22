b7636945c59a0ec67e7e00c1cef0c2397f25ba55
Z-Jarrah <jarrahzeed@gmail.com>
2020-09-21 21:29:29 -0700

    Update coeff and intercepts

--- Coding2/coding2_zj.R
+++ Coding2/coding2_zj.R
@@ -1,5 +1,6 @@
 library(MASS)
 library(glmnet)
+library(class)
 myData = Boston
 names(myData)[14] = "Y"
 iLog = c(1, 3, 5, 6, 8, 9, 10, 14);
@@ -13,6 +14,8 @@ X = as.matrix(myData[, -14])
 y = myData$Y
 lam.seq = c(0.30, 0.2, 0.1, 0.05, 0.02, 0.005)
 
+fit = lm(y~X,data=myData)
+summary(fit)
 
 one_var_lasso = function(r, x, lam) {
   xx = sum(x^2)
@@ -23,32 +26,36 @@ one_var_lasso = function(r, x, lam) {
 }
 
 MyLasso = function(X, y, lam.seq, maxit = 50) {
-  
-  # X: n-by-p design matrix without the intercept 
-  # y: n-by-1 response vector 
-  # lam.seq: sequence of lambda values 
-  # maxit: number of updates for each lambda 
+
+  # X: n-by-p design matrix without the intercept
+  # y: n-by-1 response vector
+  # lam.seq: sequence of lambda values
+  # maxit: number of updates for each lambda
   # Center/Scale X
   # Center y
-  
+
   n = length(y)
   p = dim(X)[2]
   nlam = length(lam.seq)
-  
+
   ##############################
-  # YOUR CODE: 
+  # YOUR CODE:
   # Record the corresponding means and scales
-  # For example, 
+  # For example,
   # y.mean = mean(y)
   # yc = centered y
   # Xs = centered and scaled X
+
+  y.mean = mean(y)
+  yc = y - mean(y)
+  Xs = (X - rowMeans(X)) / apply(X,2, FUN = sd)
   ##############################
-  
-  # Initilize coef vector b and residual vector r
+
+  # Initialize coefficients vector b and residual vector r
   b = rep(0, p)
   r = yc
   B = matrix(nrow = nlam, ncol = p + 1)
-  
+
   # Triple nested loop
   for (m in 1:nlam) {
     lam = 2 * n * lam.seq[m]
@@ -59,12 +66,38 @@ MyLasso = function(X, y, lam.seq, maxit = 50) {
         r = r - Xs[, j] * b[j]
       }
     }
-    B[m, ] = c(0, b)
+    B[m, ] = c(0, b)                                                            # Leading 0 to represent y-intercept???
   }
-  
+
   ##############################
   # YOUR CODE:
   # Scale back the coefficients;
   # Update the intercepts stored in B[, 1]
+  
+  
+  # Update B
+  B = t(B)
+  # Tip: its got all the coefficients from every run of lambda
+  B[1,1:6] = y.mean
+  for (row in 1:length(b)){
+    
+    for (col in 2:length(nlam)){
+  
+    }
+  }
+
+  
   ##############################
+  
+  return (B)
 }
+
+lam.seq = c(0.30, 0.2, 0.1, 0.05, 0.02, 0.005)
+lasso.fit = glmnet(X, y, alpha = 1, lambda = lam.seq)
+coef(lasso.fit)
+
+myout = MyLasso(X, y, lam.seq, maxit = 50)
+rownames(myout) = c("Intercept", colnames(X))
+myout
+
+max(abs(coef(lasso.fit) - myout))
