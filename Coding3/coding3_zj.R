9879d387893dd5c8bab0958dc3b387da98d9eece
Z-Jarrah <jarrahzeed@gmail.com>
2020-09-30 19:47:23 -0700

    Coding 3

--- Coding3/coding3_zj.R
+++ Coding3/coding3_zj.R
@@ -1,4 +1,6 @@
 #===============================================================================
+# Prepare Your Functions
+#===============================================================================
 lo.lev <- function(x1, sp){
   # x1: feature vector of length n
   # sp: a value for "span"
@@ -13,7 +15,17 @@ lo.lev <- function(x1, sp){
   # Tip: check how we compute the smoother matrix
   #      for smoothing spline models
   
+  S_mat = matrix(0,n,n)
+  Id_mat = diag(n)
+  #Fitted values
   
+  # Consider changing name of i
+  for(i in 1:n){
+    S_mat[,i] = predict(loess(Id_mat[,i] ~ x1, span = sp))
+  }
+  
+  #diagonals of S-lambda or also called span
+  lev = diag(S_mat)
   ##############################################
   
   return(lev)
@@ -27,7 +39,22 @@ onestep_CV <- function(x1, y1, sp){
   #     the corresponding residual vector
   #  2) Call lo.lev to obtain the diagonal entries of S
   #  3) Compute LOO-CV and GCV using formula from lecture notes
-  #    [lec_W5_NonlinearRegression.pdf] page 33. 
+  #    [lec_W5_NonlinearRegression.pdf] page 33.
+  
+  # 1
+  fit1 = loess(y1 ~ x1, span = sp)
+  resid_vec = resid(fit1)
+  
+  # 2
+  S_vec = lo.lev(x1, sp)
+  
+  # 3
+  # LOO-CV
+  cv = mean((resid_vec/(1-S_vec))^2)
+  
+  # GCV
+  mean_lev = mean(S_vec)
+  gcv = mean((resid_vec/(1-mean_lev))^2)
   ##############################################
   
   return(list(cv = cv, gcv = gcv))
@@ -52,6 +79,7 @@ myCV <- function(x1, y1, span){
 
 #===============================================================================
 
+# Testing Function
 mydata = read.csv(file = "Coding3_Data.csv")
 dim(mydata)
 
@@ -60,6 +88,7 @@ plot(mydata$x, mydata$y, xlab="", ylab="")
 span1 = seq(from = 0.2, by = 0.05, length = 15 )
 cv.out = myCV(mydata$x, mydata$y, span1)
 
+# Print Results
 myout = data.frame(CV = cv.out$cv, 
                    GCV = cv.out$gcv, 
                    span = span1)
@@ -69,7 +98,11 @@ myout$span[myout$GCV == min(myout$GCV)]
 
 myout$span[myout$CV == min(myout$CV)]
 
-spangcv.min = 0.5
+# Plot The Fitted Curve
+spangcv.min = myout$span[myout$GCV == min(myout$GCV)]
+spancv.min = myout$span[myout$CV == min(myout$CV)]
+
+
 plot(mydata$x, mydata$y, xlab="", ylab="", col="gray");
 fx = 1:50/50;
 fy = sin(12*(fx+0.2))/(fx+0.2)
