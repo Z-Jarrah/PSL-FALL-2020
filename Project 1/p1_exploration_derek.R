0467d47f6cff8883c085a7dbe4e04323af6ba353
Derek Chapman <chapmanderek@users.noreply.github.com>
2020-10-01 15:30:08 -1000

    Proj1 initial push

---
+++ Project 1/p1_exploration_derek.R
@@ -0,0 +1,35 @@
+#import and setup train/test data for a specifc train/test set
+j = 1
+testIDs = read.table("project1_testIDs.dat")
+data    = read.csv("Ames_data.csv")
+train_j = data[-testIDs[, j], ]
+test_j  = data[testIDs[, j], -83]
+test_jy = data[testIDs[, j], c(1,83)]
+
+# write the test files for later pipeline testing into features and response
+write.csv(train_j, file = "train.csv", row.names = F)
+write.csv(test_j,  file = "test.csv", row.names = F)
+write.csv(test_jy, file = "test_y.csv", row.names = F)
+
+###code for inclusion in mymain.R
+library(glmnet)
+
+#read in the files
+train = read.csv("train.csv")
+test  = read.csv("test.csv")
+
+
+#model 1
+y = as.numeric(unlist(train[83]))
+X = as.matrix(train[-c(1, 60, 83)])
+X = as.data.frame(train[-c(1, 60, 83)])
+thelasso = glmnet(X, y, alpha = 1)
+
+mdl_1 = lm(Sale_Price ~ ., data = train)
+summary(mdl_1)
+#model 2
+
+
+#test the models
+true_price = read.csv("test_y.csv")
+sqrt(mean((pred1 - true_price)^2))
