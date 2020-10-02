d00e3ea0a19ca932bc7bf6a5769cbb6683659473
Derek Chapman <chapmanderek@users.noreply.github.com>
2020-10-01 18:10:45 -1000

    Wrapper functions for easier testing of all splits

--- Project 1/p1_exploration_derek.R
+++ Project 1/p1_exploration_derek.R
@@ -1,17 +1,33 @@
 #import and setup train/test data for a specifc train/test set
-j = 1
-testIDs = read.table("project1_testIDs.dat")
-data    = read.csv("Ames_data.csv")
-train_j = data[-testIDs[, j], ]
-test_j  = data[testIDs[, j], -83]
-test_jy = data[testIDs[, j], c(1,83)]
+results.mdl1 = double(10)
+results.mdl2 = double(10)
+
+for(j in 1:10) {
+  testIDs = read.table("project1_testIDs.dat")
+  data    = read.csv("Ames_data.csv")
+  train_j = data[-testIDs[, j],]
+  test_j  = data[testIDs[, j],-83]
+  test_jy = data[testIDs[, j], c(1, 83)]
+  
+  #insert function to test model 1 here
+  results.mdl1[j] = mdl1_fun(train_j, test_j, test_jy)
+  results.mdl2[j] = mdl2_fun(train_j, test_j, test_jy)
+}
+
+mdl1_fun = function(train, test, test_y){
+  
+}
+
+mdl2_fun = function(train, test, test_y){
+  
+}
 
 # write the test files for later pipeline testing into features and response
 write.csv(train_j, file = "train.csv", row.names = F)
 write.csv(test_j,  file = "test.csv", row.names = F)
 write.csv(test_jy, file = "test_y.csv", row.names = F)
 
-###code for inclusion in mymain.R
+###############  code for inclusion in mymain.R  ################################
 library(glmnet)
 
 #read in the files
@@ -27,6 +43,7 @@ thelasso = glmnet(X, y, alpha = 1)
 
 mdl_1 = lm(Sale_Price ~ ., data = train)
 summary(mdl_1)
+
 #model 2
 
 
