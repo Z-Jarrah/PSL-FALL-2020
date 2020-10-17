bca5f8130a53a9e4f9d6c6023685b91de7116d0d
Derek Chapman <chapmanderek@users.noreply.github.com>
2020-10-17 12:12:45 -1000

    Include Randomforest in comparison - create plot

--- Project 1/p1_exploration_derek.R
+++ Project 1/p1_exploration_derek.R
@@ -1,12 +1,14 @@
 library(glmnet)
 library(xgboost)
+library(randomForest)
 
 #import and setup train/test data for a specifc train/test set
-results.lasso = double(10)
-results.ridge = double(10)
-results.select = double(10)
-results.slr = double(10)
-results.xgb = double(10)
+results.lasso   = double(10)
+results.ridge   = double(10)
+results.select  = double(10)
+results.slr     = double(10)
+results.xgb     = double(10)
+results.rforest = double(10)
 
 # for winsorization scores
 qs = seq(0.5, 1, by = 0.05)
@@ -20,16 +22,30 @@ for(j in 1:10) {
   test_j  = data[testIDs[, j],-83]
   test_jy = data[testIDs[, j], c(1, 83)]
   
+  
   set.seed(0271)
+  print("Lasso")
   results.lasso[j] = lasso_fun(train_j, test_j, test_jy)
+  
   set.seed(0271)
+  print("Ridge")
   results.ridge[j] = ridge_fun(train_j, test_j, test_jy)
+  
   set.seed(0271)
+  print("Elastic Net")
   results.select[j] = lasso_select_elasticnet_fun(train_j, test_j, test_jy)
+  
   set.seed(0271)
+  print("SLR")
   results.slr[j] = slr_fun(train_j, test_j, test_jy)
+  
   set.seed(0271)
+  print("XGB")
   results.xgb[j] = xgb_fun(train_j, test_j, test_jy, rounds = 500)
+  
+  set.seed(0271)
+  print("Random Forest")
+  results.rforest[j] = rand_forest_fun(train_j, test_j, test_jy, rounds = 500)
   
   #for winsorization scores
   # for(i in 1:length(qs)){
@@ -58,7 +74,19 @@ axis(1, at = seq(1, length(qs)), labels = qs)
 grid()
 }
 
-# model_types = c("Linear Regression", "Lasso", "Ridge", "Lasso to ElasticNet")
+#average score vizualization
+model_types = c("Selected Linear Regression", "Lasso", "Ridge", "Lasso to ElasticNet", 
+                "Random Forest", "XGBoost")
+scores = c(mean(results.slr), mean(results.lasso), mean(results.ridge), 
+           mean(results.select), mean(results.rforest), mean(results.xgb))
+colors = c('darkred','darkred','darkred',3,'darkred',3)
+barplot(scores, ylim = c(0, 0.18),
+        ylab = "Average RMSE over the 10 Splits",
+        col = colors)
+label_location = c(0.75, 1.9,3.1, 4.35, 5.5, 6.7)
+axis(1, at = label_location, labels = model_types)
+title(main = "Comparison of RMSE Scores Across Models", line=0)
+abline(h = 0.125, lty = 'dashed', col = 'black')
 
 drop_levels = function(df, mdl){
   lvls_names = names(mdl$xlevels)
@@ -371,6 +399,13 @@ xgb_fun = function(train, test, test_y, rounds = 500){
   y = as.numeric(unlist(train[83]))
   X = train[, -c(1, 83)]
   
+  X = subset(X, select = -c(Garage_Yr_Blt, Street, Utilities,  Condition_2, Roof_Matl, 
+                            Heating, Pool_QC, Misc_Feature, Low_Qual_Fin_SF,
+                            Pool_Area, Longitude, Latitude))
+  test = subset(test, select = -c(Garage_Yr_Blt, Street, Utilities,  Condition_2, Roof_Matl, 
+                                  Heating, Pool_QC, Misc_Feature, Low_Qual_Fin_SF,
+                                  Pool_Area, Longitude,Latitude))
+  
   winsor.vars <- c("Lot_Frontage", "Lot_Area", "Mas_Vnr_Area", "BsmtFin_SF_2", "Bsmt_Unf_SF", "Total_Bsmt_SF", "Second_Flr_SF", 'First_Flr_SF', "Gr_Liv_Area", "Garage_Area", "Wood_Deck_SF", "Open_Porch_SF", "Enclosed_Porch", "Three_season_porch", "Screen_Porch", "Misc_Val")
   quan.value <- 0.95
   for(var in winsor.vars){
@@ -434,4 +469,78 @@ xgb_fun = function(train, test, test_y, rounds = 500){
   xgb_pred = predict(xgb.model, newdata=test_mat)
   
   return(sqrt(mean((log(test_y$Sale_Price) - xgb_pred)^2)))
+}
+
+#random forest
+rand_forest_fun = function(train, test, test_y, rounds = 500){
+  train[, 83] = log(train[, 83])
+  train$Sale_Type[train$Sale_Type == "WD "] = "WD"
+  y = as.numeric(unlist(train[83]))
+  X = train[, -c(1, 83)]
+  
+  X = subset(X, select = -c(Garage_Yr_Blt, Street, Utilities,  Condition_2, Roof_Matl, 
+                            Heating, Pool_QC, Misc_Feature, Low_Qual_Fin_SF,
+                            Pool_Area, Exterior_1st, Exterior_2nd, Longitude, Latitude))
+  test = subset(test, select = -c(Garage_Yr_Blt, Street, Utilities,  Condition_2, Roof_Matl, 
+                                  Heating, Pool_QC, Misc_Feature, Low_Qual_Fin_SF,
+                                  Pool_Area, Longitude,Latitude))
+  
+  winsor.vars <- c("Lot_Frontage", "Mas_Vnr_Area", "BsmtFin_SF_2", 
+                   "Bsmt_Unf_SF", "Total_Bsmt_SF", "Second_Flr_SF", 'First_Flr_SF', 
+                   "Gr_Liv_Area", "Garage_Area", "Wood_Deck_SF", "Open_Porch_SF", 
+                   "Enclosed_Porch", "Three_season_porch", "Screen_Porch", "Misc_Val")
+  quan.value <- 0.95
+  for(var in winsor.vars){
+    tmp <- X[, var]
+    myquan <- quantile(tmp, probs = quan.value, na.rm = TRUE)
+    tmp[tmp > myquan] <- myquan
+    X[, var] <- tmp
+    
+    tmp <- test[, var]
+    myquan <- quantile(tmp, probs = quan.value, na.rm = TRUE)
+    tmp[tmp > myquan] <- myquan
+    test[, var] <- tmp
+  }
+  
+  #process training data
+  categorical.vars <- colnames(X)[
+    which(sapply(X, function(x) mode(x)=="character"))]
+  train.matrix <- X[, !colnames(X) %in% categorical.vars, drop=FALSE]
+  saved_levels = list()
+  n.train <- nrow(train.matrix)
+  for(var in categorical.vars){
+    mylevels <- sort(unique(X[, var]))
+    saved_levels[[var]] = mylevels
+    m <- length(mylevels)
+    m <- ifelse(m>2, m, 1)
+    tmp.train <- matrix(0, n.train, m)
+    col.names <- NULL
+    for(j in 1:m){
+      tmp.train[X[, var]==mylevels[j], j] <- 1
+      col.names <- c(col.names, paste(var, '_', mylevels[j], sep=''))
+    }
+    colnames(tmp.train) <- col.names
+    train.matrix <- cbind(train.matrix, tmp.train)
+  }
+  
+  #process test data
+  test.matrix <- test[, !colnames(test) %in% categorical.vars, drop=FALSE]
+  n.test <- nrow(test.matrix)
+  for(var in categorical.vars){
+    mylevels = saved_levels[[var]]
+    m = length(mylevels)
+    m <- ifelse(m>2, m, 1)
+    temp_test <- matrix(0, n.test, m)
+    col.names <- NULL
+    for(j in 1:m){
+      temp_test[test[, var]==mylevels[j], j] <- 1
+      col.names <- c(col.names, paste(var, '_', mylevels[j], sep=''))
+    }
+    colnames(temp_test) <- col.names
+    test.matrix <- cbind(test.matrix, temp_test)
+  }
+  
+  fit = randomForest(y ~ ., data=train.matrix, ntree=100)
+  rf_pred = predict(fit, test.matrix)
+  return(sqrt(mean((log(test_y$Sale_Price) - rf_pred)^2)))
 }
