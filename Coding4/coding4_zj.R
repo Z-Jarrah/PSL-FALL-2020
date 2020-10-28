f152dd1aec31b95d32882327ff3ae331ad11f846
Z-Jarrah <jarrahzeed@gmail.com>
2020-10-27 21:19:22 -0700

    First draft code equations

--- Coding4/coding4_zj.R
+++ Coding4/coding4_zj.R
@@ -2,23 +2,89 @@ library(mclust)
 
 # initial values
 # Just for testing
-pi1 = 0.5
-pi2 = 0.5
+pi1 = para0$prob[1]
+pi2 = para0$prob[2]
 mu1 = -0.01
 mu2 = 0.01
 sigma1 = sqrt(0.01)
 sigma2 = sqrt(0.02)
 
+# === E-step ===
+#Prob of G=1 given obs
+# Use para instead of para0
+# para0$prob[1]*dmvnorm(faithful[1,], para0$mean[,1], para0$Sigma)/
+# (para0$prob[1]*dmvnorm(faithful[1,], para0$mean[,1], para0$Sigma) + para0$prob[2]*dmvnorm(faithful[1,], para0$mean[,2], para0$Sigma))
+
+prob1_vec = para0$prob[1]*dmvnorm(faithful, para0$mean[,1], para0$Sigma)/
+  (para0$prob[1]*dmvnorm(faithful, para0$mean[,1], para0$Sigma) + para0$prob[2]*dmvnorm(faithful, para0$mean[,2], para0$Sigma))
+
+# prob2_vec = 1 - prob1_vec
+prob_matrix = cbind(prob1_vec, 1-prob1_vec)
+# === E-step ===
+
 Estep <- function (data , G , para ) {
   # Your Code
-  # Compute 
+  prob1_vec = para$prob[1]*dmvnorm(faithful, para$mean[,1], para$Sigma)/
+    (para$prob[1]*dmvnorm(faithful, para$mean[,1], para$Sigma) + para$prob[2]*dmvnorm(faithful, para$mean[,2], para$Sigma))
+  
+  # prob2_vec = 1 - prob1_vec
+  # prob_matrix = cbind(prob1_vec, 1-prob1_vec)
   
   # Return the n-by -G probability matrix
-  
+  return (cbind(prob1_vec, 1-prob1_vec))
 }
+
+# === m-step ===
+prob = apply(prob_matrix, 2, mean)
+
+rnk_Xn1 = prob_matrix[,1]*faithful
+mean1 = apply(rnk_Xn1, 2,sum)/apply(prob_matrix,2,sum)[1]
+rnk_Xn2 = prob_matrix[,2]*faithful
+mean2 = apply(rnk_Xn2, 2,sum)/apply(prob_matrix,2,sum)[2]
+updated_mean = cbind(mean1, mean2)
+
+# Replace '4' with columns in dataset ^2
+# Replace 2 with columns of dataset
+mat_temp1 = matrix(rep(0, 4), nrow=2)
+
+for (i in 1:nrow(faithful)) {
+  x1 = prob_matrix[i,1] * (t(faithful[i,]-mean1) %*% t(t(faithful[i,]-mean1)))
+  x2 = prob_matrix[i,2] * (t(faithful[i,]-mean2) %*% t(t(faithful[i,]-mean2)))
+  
+  mat_temp1 = mat_temp1 + x1 + x2
+}
+
+covar_matrix = mat_temp1/nrow(faithful)
+
+# === m-step ===
+
 Mstep <- function (data , G , para , post.prob ) {
   # Your Code
+  prob = apply(prob_matrix, 2, mean)
+  
+  rnk_Xn1 = prob_matrix[,1]*faithful
+  mean1 = apply(rnk_Xn1, 2,sum)/apply(prob_matrix,2,sum)[1]
+  rnk_Xn2 = prob_matrix[,2]*faithful
+  mean2 = apply(rnk_Xn2, 2,sum)/apply(prob_matrix,2,sum)[2]
+  updated_mean = cbind(mean1, mean2)
+  
+  # Replace '4' with columns in dataset ^2
+  # Replace 2 with columns of dataset
+  mat_temp1 = matrix(rep(0, 4), nrow=2)
+  
+  for (i in 1:nrow(faithful)) {
+    x1 = prob_matrix[i,1] * (t(faithful[i,]-mean1) %*% t(t(faithful[i,]-mean1)))
+    x2 = prob_matrix[i,2] * (t(faithful[i,]-mean2) %*% t(t(faithful[i,]-mean2)))
+    
+    mat_temp1 = mat_temp1 + x1 + x2
+  }
+  
+  covar_matrix = mat_temp1/nrow(faithful)
+  
+  
   # Return the updated parameters
+  
+  return(list(prob= prob, mean= updated_mean, Sigma=covar_matrix))
 }
 myEM <- function (data , itmax , G , para ) {
   # itmax: num of iterations
@@ -63,3 +129,4 @@ Rout <- em ( modelName = "EEE", data = faithful ,
              parameters = ini0 )$parameters
 
 list ( Rout$pro , Rout$mean , Rout$variance$Sigma )
+
