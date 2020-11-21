f92aaea03fa1ac05ca323fe9c4dacb39e8a283ed
Derek Chapman <chapmanderek@users.noreply.github.com>
2020-11-21 12:15:52 -1000

    WIP mymain - not functional with set vocab

---
+++ Project 3/split_1/mymain.R
@@ -0,0 +1,55 @@
+set.seed(1234)
+myvocab <- scan(file = "myvocab.txt", what = character())
+
+# Construct DocumentTerm matrix (max 4-grams)
+stop_words = c("i", "me", "my", "myself", 
+               "we", "our", "ours", "ourselves", 
+               "you", "your", "yours", 
+               "their", "they", "his", "her", 
+               "she", "he", "a", "an", "and",
+               "is", "was", "are", "were", 
+               "him", "himself", "has", "have", 
+               "it", "its", "the", "us")
+# it_word_select = itoken(train$review,
+#                   preprocessor = tolower, 
+#                   tokenizer = word_tokenizer)
+# tmp.vocab = create_vocabulary(it_word_select,
+#                               stopwords = stop_words,
+#                               ngram = c(1L,4L))
+# tmp.vocab = prune_vocabulary(tmp.vocab, term_count_min = 10,
+#                              doc_proportion_max = 0.5,
+#                              doc_proportion_min = 0.001)
+# dtm_word_select  = create_dtm(it_word_select, vocab_vectorizer(tmp.vocab))
+
+
+# Load training
+train = read.table("train.tsv", stringsAsFactors = FALSE, header = TRUE)
+train$review = gsub('<.*?>', ' ', train$review)
+it_train = itoken(train$review,
+                  preprocessor = tolower,
+                  tokenizer = word_tokenizer)
+dtm_train = create_dtm(it_train, vocab_vectorizer(myvocab))
+
+#Load test
+test = read.table("test.tsv", stringsAsFactors = FALSE, header = TRUE)
+test$review = gsub('<.*?>', ' ', test$review)
+it_test = itoken(test$review,
+                  preprocessor = tolower,
+                  tokenizer = word_tokenizer)
+dtm_test = create_dtm(it_test, vocab_vectorizer(myvocab))
+
+# Train classification model
+class_mdl = glmnet(
+  x = dtm_train,
+  y = train$sentiment,
+  alpha = 0,
+  family = 'binomial'
+  )
+
+# Compute and save predictions
+mypred = predict(mylogit.fit, class_mdl, type = "response")
+output = data.frame(id = test$id, prob = as.vector(mypred))
+write.table(output,
+            file = "mysubmission.txt",
+            row.names = FALSE,
+            sep = '\t')
