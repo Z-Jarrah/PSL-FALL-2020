bff8d5b956e1603c11db1abb40caced5143c58e9
Derek Chapman <chapmanderek@users.noreply.github.com>
2020-11-22 12:06:39 -1000

    updated vocab building and additional exploration files

--- Project 3/project3_explore_mor_data.R
+++ Project 3/project3_explore_mor_data.R
@@ -6,52 +6,8 @@ library(pROC)
 
 setwd("~/Google Drive/Geek2/UofIll/CS598_PSL/Github/PSL-FALL-2020/Project 3")
 
-# ==============================================================================
-# === Creating the Test IDs ====================================================
-# ==============================================================================
-data <- read.table("alldata.tsv", stringsAsFactors = FALSE,
-                   header = TRUE)
-
-testIDs <- read.csv("splits_F20.csv", header = TRUE)
-for(j in 1:5){
-  dir.create(paste("split_", j, sep=""))
-  train <- data[-testIDs[,j], c("id", "sentiment", "review") ]
-  test <- data[testIDs[,j], c("id", "review")]
-  test.y <- data[testIDs[,j], c("id", "sentiment", "score")]
-  
-  tmp_file_name <- paste("split_", j, "/", "train.tsv", sep="")
-  write.table(train, file=tmp_file_name, 
-              quote=TRUE, 
-              row.names = FALSE,
-              sep='\t')
-  tmp_file_name <- paste("split_", j, "/", "test.tsv", sep="")
-  write.table(test, file=tmp_file_name, 
-              quote=TRUE, 
-              row.names = FALSE,
-              sep='\t')
-  tmp_file_name <- paste("split_", j, "/", "test_y.tsv", sep="")
-  write.table(test.y, file=tmp_file_name, 
-              quote=TRUE, 
-              row.names = FALSE,
-              sep='\t')
-}
-# ==============================================================================
-# ==============================================================================
-# ==============================================================================
-
-
-# ==============================================================================
-# === Binary Classification ====================================================
-# ==============================================================================
-
-#####################################
-# Load your vocabulary and training data
-#####################################
-
-# Delete
-# myvocab <- scan(file = "myvocab.txt", what = character())
+# select the words we might want to try
 # What we've tried (I)
-# Construct DocumentTerm matrix (max 4-grams)
 stop_words = c("i", "me", "my", "myself", 
                "we", "our", "ours", "ourselves", 
                "you", "your", "yours", 
@@ -68,19 +24,41 @@ tmp.vocab = create_vocabulary(it_word_select,
                               ngram = c(1L,4L))
 tmp.vocab = prune_vocabulary(tmp.vocab, term_count_min = 10,
                              doc_proportion_max = 0.5,
-                             doc_proportion_min = 0.001)
+                             doc_proportion_min = 0.01)
 dtm_word_select  = create_dtm(it_word_select, vocab_vectorizer(tmp.vocab))
 
 #select words
-tmpfit = glmnet(x = dtm_word_select, 
-                y = train$sentiment, 
-                alpha = 1,
-                family='binomial')
+tmpfit_cv = cv.glmnet(x = dtm_word_select, 
+                      y = train$sentiment, 
+                      alpha = 1,
+                      family='binomial')
+
+selected_fit = glmnet(x = dtm_word_select, 
+                      y = train$sentiment, 
+                      alpha = 0,
+                      lambda = tmpfit_cv$lambda.1se,
+                      family='binomial')
+
+length(selected_fit$beta)
+selected_words = colnames(dtm_word_select)[which(abs(selected_fit$beta) > .5)]
+# write.table(selected_words, "additional_words.txt", append = F, 
+#             eol = "\n", 
+#             row.names = F, col.names = F)
+
+current_vocab = scan(file = "split_2/myvocab.txt", what = character())
+for(word in selected_words){
+  if(!(word %in% current_vocab)){
+    print(paste(word, word %in% current_vocab))
+  }
+}
+
 myvocab = colnames(dtm_word_select)[which(tmpfit$beta[, 36] != 0)]
 my_vectorizer = vocab_vectorizer(create_vocabulary(myvocab, 
                                                 ngram = c(1L, 2L)))
 
 
+
+# creating mysubmission for each split
 for(j in 1:5){
   set.seed(1234)
   print(paste('starting on split ', j))
@@ -137,8 +115,7 @@ for(j in 1:5){
   setwd('..')
 }
 
-# ==============================================================================
-
+# scoring the classifier ---
 # source(mymain.R)
 auc_scores = rep(0, 5)
 for(i in 1:5){
@@ -155,3 +132,32 @@ for(i in 1:5){
 }
 
 print(auc_scores)
+
+
+# === Creating the splits ====================================================
+data <- read.table("alldata.tsv", stringsAsFactors = FALSE,
+                   header = TRUE)
+
+testIDs <- read.csv("splits_F20.csv", header = TRUE)
+for(j in 1:5){
+  dir.create(paste("split_", j, sep=""))
+  train <- data[-testIDs[,j], c("id", "sentiment", "review") ]
+  test <- data[testIDs[,j], c("id", "review")]
+  test.y <- data[testIDs[,j], c("id", "sentiment", "score")]
+  
+  tmp_file_name <- paste("split_", j, "/", "train.tsv", sep="")
+  write.table(train, file=tmp_file_name, 
+              quote=TRUE, 
+              row.names = FALSE,
+              sep='\t')
+  tmp_file_name <- paste("split_", j, "/", "test.tsv", sep="")
+  write.table(test, file=tmp_file_name, 
+              quote=TRUE, 
+              row.names = FALSE,
+              sep='\t')
+  tmp_file_name <- paste("split_", j, "/", "test_y.tsv", sep="")
+  write.table(test.y, file=tmp_file_name, 
+              quote=TRUE, 
+              row.names = FALSE,
+              sep='\t')
+}
